5dc3330924ded10ef4bb217218b849fe6c2710af
Priyanshu_Gairola <pgairola9945@gmail.com>
2026-09-20 14:02:23 +0530

    Remove STL and used pure c and c++ concepts
    Refactored optimizeresult to use custom IntArray for task IDs and added methods for managing scheduled and deferred task IDs.

--- Priority_Based_Workflow_Optimizer_1/include/cpp/optimizer.h
+++ Priority_Based_Workflow_Optimizer_1/include/cpp/optimizer.h
@@ -1,11 +1,56 @@
 #ifndef OPTIMIZER_H
 #define OPTIMIZER_H
-#include <vector>
 
 #include "../member1_graph/graph.h"
 #include "../member2_scheduletask/taskmanager.h"
-#include "../member2_scheduletask/schedulemanager"
+#include "../member2_scheduletask/schedulemanager.h"
 using namespace std;
+
+class IntArray{        // custom dynamic integer array
+    private:
+        int* data;
+        int size;
+        int capacity;
+    public:
+        IntArray(){
+            size = 0;
+            capacity = 10;
+            data = new int[capacity];
+        }
+        
+        ~IntArray(){
+            delete[] data;
+        }
+        
+        void add(int value){
+            if (size == capacity){
+                capacity = capacity * 2;
+                int* newData = new int[capacity];
+                for (int i = 0; i < size; i++)
+                {
+                    newData[i] = data[i];
+                }
+                delete[] data;
+                data = newData;
+            }
+            data[size] = value;
+            size++;
+        }
+        
+        int getSize() const{
+            return size;
+        }
+        
+        int get(int index) const{
+            if (index < 0 || index >= size){
+                return -1;
+            }
+            return data[index];
+        }
+        void clear(){
+            size = 0;
+        }
+};
 
 struct optimizeresult{       //store final result of optimization
     int total_tasks;
@@ -17,8 +62,8 @@ struct optimizeresult{       //store final result of optimization
     int used_minutes;
     double time_utilization;
     double priority_percent;
-    vector<int> schedule_id;
-    vector<int> deferred_id;
+    IntArray schedule_id;
+    IntArray deferred_id;
     
     optimizeresult(){       //constructor
         total_tasks=0;
@@ -31,9 +76,28 @@ struct optimizeresult{       //store final result of optimization
         time_utilization=0.0;
         priority_percent=0.0;
     }
+
+    void addScheduledTaskId(int id){
+        schedule_id.add(id);
+    }
+    void addDeferredTaskId(int id){
+        deferred_id.add(id);
+    }
+    int getScheduledCount() const{
+        return schedule_id.getSize();
+    }
+    int getDeferredCount() const{
+        return deferred_id.getSize();
+    }
+    int getScheduledId(int index) const{
+        return schedule_id.get(index);
+    }
+    int getDeferredId(int index) const{
+        return deferred_id.get(index);
+    }
 };
 
-class WorkflowOptimizer{
+class WorkflowOptimizer{         //workflow optimizer
     public:
         WorkflowOptimizer();   //constructor
         optimizeresult optimize_workflow(        //optimize workflow
@@ -43,5 +107,10 @@ class WorkflowOptimizer{
         void print_report(
             const optimizeresult& result, const TaskManager& task_mgr
         )const;
+        private:
+            Task* findHighestPriorityTask(
+            Task** possibleTasks,
+            int count
+        );
 };
 #endif
